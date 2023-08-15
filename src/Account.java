0c468f3d58ed4a3e08311749c4b6e4e670f4894f
Sean <51032338+Seanm64@users.noreply.github.com>
2023-08-15 14:19:43 -0400

    Initial commit

---
+++ src/Account.java
@@ -0,0 +1,32 @@
+
+public class Account {
+	private int money, secretCounter, last4SSN;
+	private double interestRate;
+	private String name, SSN, password, email;
+	
+	public Account(String name, String SSN, String password, String email)
+	{
+		this.name = name;
+		this.SSN = SSN;
+		this.password = password;
+		this.email = email;
+		secretCounter = 0;
+		money = 0;
+		interestRate = 0.1;
+		
+		last4SSN = getLast4SSN(); //Used for security measures
+	}
+	
+	/** @return last 4 digits of the user's SSN */
+	private int getLast4SSN() {
+		int len = SSN.length();		
+
+		return Integer.parseInt(SSN.substring(len-5, len-1));
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+	
+}
